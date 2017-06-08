afcd281582e54d928f553b765425a15ee69a490d
Yuanze Chen <chenyua@in.tum.de>
2017-06-08 15:36:00 +0200

    orz

--- binary_segmentation/sheet5_ex3.m
+++ binary_segmentation/sheet5_ex3.m
@@ -15,37 +15,7 @@ K = 5;
 %% foreground
 img_fg = img(x1:x2,y1:y2,:);
 img_fg = reshape(img_fg,(x2+1-x1)*(y2+1-y1), 3);
-<<<<<<< HEAD
 [mu_fg, sigma_fg, pi_fg] = multi_gaussian(K, itermax, img_fg);
-=======
-[m, n] = size(img_fg);
-gama_fg = zeros(m,5);
-pi_fg = 0.2*ones(5,1);
-mu_ulti = sum(img_fg)./m;
-mu_fg = repmat(mu_ulti, 5, 1);
-sigma_fg = zeros(3,3,5);
-for i = 1:5
-    sigma_fg(:,:,i) = eye(3); 
-end
-
-for iter = 1:itermax
-    prob = prob_eval(img_fg, mu_fg, sigma_fg, pi_fg, 0);
-    for k = 1:5
-        % update gama
-        gama_fg(:,k) = prob_eval(img_fg, mu_fg, sigma_fg, pi_fg, k)./prob;
-        % gama_ulti is redundant
-        gama_ulti = repmat(gama_fg(:,k),1,3);
-        mu_fg(k,:) = sum(gama_ulti.*img_fg, 1)./sum(gama_fg(:,k));
-        temp = img_fg-repmat(mu_fg(k,:), m, 1);
-        temp1 = temp.*gama_ulti;
-        sigma_fg(:,:,k) = temp1'*temp./sum(gama_fg(:,k));
-    end
-    for k = 1:5
-        pi_fg(k) = sum(gama_fg(:,k))/sum(sum(gama_fg));
-    end
-end
->>>>>>> 295127549ee31a4d5d35d1371da9acb20dc30c27
-
 %% background
 img_bg1 = img(:,1:x1-1,:);
 img_bg1 = reshape(img_bg1,(x1-1)*480, 3);
@@ -66,3 +36,12 @@ img_binary = prob_bg<prob_fg;
 img_result = reshape(img_binary, 480, 640);
 figure(2);
 imshow(img_result);
+
+%% maxflow
+mask = 1:M*N;
+mask = reshape(mask, M, N);
+in_mask = zeros(M-2, N-2, 4);
+in_mask(:,:,1) = mask(1:M-2,2:N-1);
+in_mask(:,:,2) = mask(3:M,2:N-1);
+in_mask(:,:,3) = mask(2:M-1,1:N-2);
+in_mask(:,:,4) = mask(2:M-1,3:N);
