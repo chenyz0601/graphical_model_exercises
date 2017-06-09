bf28d2cfa15b1ac64d53c75fbe2a71e583b731d3
Yuanze Chen <chenyua@in.tum.de>
2017-06-09 17:42:31 +0200

    binary segmentaion done

--- binary_segmentation/multi_gaussian.m
+++ binary_segmentation/multi_gaussian.m
@@ -1,23 +1,26 @@
 function [mu, sigma , pi]= multi_gaussian(K, itermax, X)
-[m, n] = size(X);
-gama = zeros(m,K);
-pi = 0.2*ones(K,1);
-mu_ulti = sum(X)./m;
-mu = repmat(mu_ulti, K, 1);
-sigma = zeros(3,3,K);
-for i = 1:K
-    sigma(:,:,i) = eye(3); 
-end
+    [m, n] = size(X);
+    gama = zeros(m,K);
+    pi = 0.2*ones(K,1);
+    mu_ulti = sum(X)./m;
+    mu = repmat(mu_ulti, K, 1);
+    %mu = rand(K,3);
+    sigma = zeros(3,3,K);
+    for i = 1:K
+        sigma(:,:,i) = eye(3); 
+    end
 
-for iter = 1:itermax
-    prob = prob_eval(X, mu, sigma, pi, 0);
-    for k = 1:K
-        gama(:,k) = prob_eval(X, mu, sigma, pi, k)./prob;
-        mu(k,:) = gama(:,k)'*X./sum(gama(:,k));
-        sigma(:,:,k) = X'*spdiags(gama(:,k),0,m,m)*X./sum(gama(:,k));
+    for iter = 1:itermax
+        prob = prob_eval(X, mu, sigma, pi, 0);
+        %gama_old = gama;
+        for k = 1:K
+            gama(:,k) = prob_eval(X, mu, sigma, pi, k)./prob;
+            mu(k,:) = gama(:,k)'*X./sum(gama(:,k));
+            sigma(:,:,k) = X'*spdiags(gama(:,k),0,m,m)*X./sum(gama(:,k));
+        end
+        % disp(norm(gama-gama_old));
+        for k = 1:K
+            pi(k) = sum(gama(:,k))/sum(sum(gama));
+        end
     end
-    for k = 1:K
-        pi(k) = sum(gama(:,k))/sum(sum(gama));
-    end
-end
 end
