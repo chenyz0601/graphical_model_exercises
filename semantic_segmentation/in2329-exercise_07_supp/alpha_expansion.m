a36e5a2c7dbaa74f7d35e4e0dc623427b0fdce01
Yuanze Chen <chenyua@in.tum.de>
2017-07-10 16:44:09 +0200

    add new assignments

--- semantic_segmentation/in2329-exercise_07_supp/alpha_expansion.m
+++
@@ -1,6 +0,0 @@
-raw_img = imread('1_9_s.bmp');
-raw_img = im2double(raw_img);
-[H,W,C] = size(raw_img);
-file_name = fopen('1_9_s.c_unary');
-precision = 'ubit1';
-unary = fread(file_name,[21,H*W],precision);
--- semantic_segmentation/in2329-exercise_07_supp/pair_energy.m
+++
@@ -1,7 +0,0 @@
-function out = pair_energy(yi,yj,xi,xj)
-    if yi == yj
-        out = 0;
-    else
-        out = exp(-0.5*norm(xi-xj)^2);
-    end
-end
